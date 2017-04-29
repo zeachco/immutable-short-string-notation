ddbf8294bf9bface78e7dc57b875fb3212d71bd9
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-04-29 11:08:36 -0400

    code reusability

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { Collection, Map } from 'immutable';
 
-const forceFirstParamToArray = (target, fn) => {
+const forceParamToArray = (target, fn, argIndex = 0) => {
     const originalFn = '__' + fn;
     target.prototype[originalFn] = target.prototype[fn];
     target.prototype[fn] = function(...args) {
-        if (typeof args[0] === 'string') args[0] = args[0].split('.');
+        if (typeof args[argIndex] === 'string') args[argIndex] = args[argIndex].split('.');
         return target.prototype[originalFn].apply(this, args);
     }
 }
 
 // This allows to use Immutable.Map.getIn('path.path2.path3')
-forceFirstParamToArray(Collection, 'getIn');
+forceParamToArray(Collection, 'getIn');
 // This allows to use Immutable.Map.setIn('path.path2.path3')
-forceFirstParamToArray(Map, 'setIn');
+forceParamToArray(Map, 'setIn');
