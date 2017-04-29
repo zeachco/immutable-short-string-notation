1561e0924c4dc35583f71389ba9bae1ba38aea54
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-04-29 11:19:50 -0400

    es5

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import { Collection, Map } from 'immutable';
+var Immutable = require('immutable');
 
-const forceParamToArray = (target, fn, argIndex = 0) => {
-    const originalFn = '__' + fn;
+function forceParamToArray(target, fn, argIndex) {
+    argIndex = argIndex || 0;
+    var originalFn = '__' + fn;
     target.prototype[originalFn] = target.prototype[fn];
-    target.prototype[fn] = function(...args) {
-        if (typeof args[argIndex] === 'string') args[argIndex] = args[argIndex].split('.');
-        return target.prototype[originalFn].apply(this, args);
+    target.prototype[fn] = function() {
+        if (typeof arguments[argIndex] === 'string') arguments[argIndex] = arguments[argIndex].split('.');
+        return target.prototype[originalFn].apply(this, arguments);
     }
 }
 
 // This allows to use Immutable.Map.getIn('path.path2.path3')
-forceParamToArray(Collection, 'getIn');
+forceParamToArray(Immutable.Collection, 'getIn');
 // This allows to use Immutable.Map.setIn('path.path2.path3')
-forceParamToArray(Map, 'setIn');
+forceParamToArray(Immutable.Map, 'setIn');
